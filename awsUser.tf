28bccb609cf5680809a654e78ec506c2962b8c5f
Pardeep Chahar <75135128+PardeepChahar@users.noreply.github.com>
2021-08-30 23:36:35 +0530

    Update awsUser.tf

--- awsUser.tf
+++ awsUser.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA2VGUDXUJURNMJ5CD"
-  secret_key = "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"
+  access_key = "None"
+  secret_key = "None"
 }
